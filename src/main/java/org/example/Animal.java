0b4a15ca9aa646d598ef9be1fba39dc789a640df
Alexander Zhuravlev <warcueid@mail.ru>
2024-10-21 21:30:56 +0400

    7) Changed class animal to boolean fur type

--- src/main/java/org/example/Animal.java
+++ src/main/java/org/example/Animal.java
@@ -3,30 +3,35 @@ package org.example;
 public class Animal {
     private final String species;
     private final String eyeColor;
-    private final int furBulk;
+    private final boolean fur;
 
     public Animal(AnimalBuilder builder) {
         this.species = builder.species;
         this.eyeColor = builder.eyeColor;
-        this.furBulk = builder.furBulk;
+        this.fur = builder.fur;
     }
 
     @Override
     public String toString() {
-        return "Animal [species = " + species + ", Bulks of fur = " + furBulk + ", Eye Color = " + eyeColor + "]";
+        return "Animal [species = " + species + ",  Fur = " +  fur + ", Eye Color = " + eyeColor + "]";
     }
 
     public static class AnimalBuilder {
         private final String species;
         private final String eyeColor;
-        private final int furBulk;
+        private boolean fur;
 
 
-        public AnimalBuilder(String species, String eyeColor, int furBulk) {
+        public AnimalBuilder(String species, String eyeColor) {
             this.species = species;
             this.eyeColor = eyeColor;
-            this.furBulk = furBulk;
         }
+
+        public AnimalBuilder fur(boolean fur) {
+            this.fur = fur;
+            return this;
+        }
+
         public Animal build() {
             return new Animal(this);
         }
@@ -35,7 +40,7 @@ public class Animal {
 
 class animalBuild {
     public static void main(String[] args) {
-        Animal animal = new Animal.AnimalBuilder("mammals", "Hazel", 5)
+        Animal animal = new Animal.AnimalBuilder("mammals", "Hazel").fur(false).fur(true)
                 .build();
         System.out.println(animal);
     }
