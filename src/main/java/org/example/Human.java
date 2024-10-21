9829cf9618dea90759d588cfb26db37825fd01ca
Alexander Zhuravlev <warcueid@mail.ru>
2024-10-21 17:49:21 +0400

    2) Changed class human

--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -1,4 +1,43 @@
 package org.example;
 
 public class Human {
+    private final String gender;
+    private final String secondName;
+    private final int age;
+
+    public Human(HumanBuilder builder) {
+        this.gender = builder.gender;
+        this.secondName = builder.secondName;
+        this.age = builder.age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human [gender=" + gender + ",age=" + age + ",secondName=" + secondName + "]";
+    }
+
+    public static class HumanBuilder {
+        private final String gender;
+        private final String secondName;
+        private final int age;
+
+
+        public HumanBuilder(String gender, String secondName, int age) {
+            this.gender = gender;
+            this.secondName = secondName;
+            this.age = age;
+        }
+        public Human build() {
+            return new Human(this);
+        }
+    }
 }
+
+class main {
+    public static void main(String[] args) {
+        Human human = new Human.HumanBuilder("Male", "Bishop", 25)
+                .build();
+        System.out.println(human);
+    }
+}
+
