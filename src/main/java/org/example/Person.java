00cfc0c864756b1b5253b21d6553cb69753f0219
Naif <knifffe@yandex.ru>
2024-10-21 14:53:14 +0300

    Create Person.java

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,47 @@
+public class Person implements Comparable<Person>{
+    private String gender;
+    private String surname;
+    private Integer age;
+
+    // Builder Pattern
+    public static class Builder {
+        private String gender;
+        private String surname;
+        private Integer age;
+
+        public Person.Builder gender(String gender) {
+            this.gender = gender;
+            return this;
+        }
+
+        public Person.Builder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Person.Builder age(Integer age) {
+            this.age = age;
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    private Person(Person.Builder builder) {
+        this.gender = builder.gender;
+        this.surname = builder.surname;
+        this.age = builder.age;
+    }
+
+    // Геттеры и compareTo для сортировки
+    public String getGender() {
+        return gender;
+    }
+
+    @Override
+    public int compareTo(Person other) {
+        return this.gender.compareTo(other.gender);
+    }
+}
