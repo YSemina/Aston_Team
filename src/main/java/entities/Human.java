7fc24eaccea2587fcd6045265fa31f473967b8dd
Alexander Zhuravlev <warcueid@mail.ru>
2024-10-24 22:18:09 +0400

    Test

--- src/main/java/entities/Human.java
+++ src/main/java/entities/Human.java
@@ -2,7 +2,7 @@ package entities;
 
 import java.util.Scanner;
 import java.io.*;
-import java.util.*;
+import java.util.Random;
 
 public class Human {
     private final String gender;
@@ -47,4 +47,112 @@ public class Human {
     }
 }
 
+class Filler {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select how to fill class :");
+        System.out.println("1 - Autocomplete (random values)");
+        System.out.println("2 - Manual entry");
+        System.out.println("3 - Filling from file");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                RandomFill randomFill = new RandomFill();
+                randomFill.execute();
+                break;
+            case 2:
+                ManualFill manualFill = new ManualFill();
+                manualFill.execute();
+                break;
+            case 3:
+                FFFill fFFill = new FFFill();
+                fFFill.execute();
+                break;
+            default:
+                System.out.println("Wrong choice!");
+                break;
+        }
+    }
+
+    static class RandomFill {
+        void execute() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter number of humans: ");
+            int repeatCount = scanner.nextInt();
+            scanner.nextLine();
+
+            for (int i = 0; i < repeatCount; i++) {
+                String[] gender = {"Male", "Female"};
+                String[] secondName = {"Woods", "Dauda", "Hackett", "Bishop"};
+                Random random = new Random();
+
+                Human human = new Human.HumanBuilder()
+                        .setGender(gender[random.nextInt(gender.length)])
+                        .setAge(random.nextInt(99))
+                        .setSecondName(secondName[random.nextInt(secondName.length)])
+                        .build();
+
+                System.out.println(human);
+            }
+        }
+    }
+
+    static class ManualFill {
+        void execute() {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Enter number of humans: ");
+            int repeatCount = scanner.nextInt();
+            scanner.nextLine();
+
+            for (int i = 0; i < repeatCount; i++) {
+
+                Human.HumanBuilder humanBuilder = new Human.HumanBuilder();
+
+                System.out.print("Enter gender: ");
+                String gender = scanner.nextLine();
+                humanBuilder.setGender(gender);
+
+                System.out.print("Enter Second Name: ");
+                String secondName = scanner.nextLine();
+                humanBuilder.setSecondName(secondName);
+
+                System.out.print("Enter age:  ");
+                int age = Integer.parseInt(scanner.nextLine());
+                humanBuilder.setAge(age);
+
+                Human human = humanBuilder.build();
+
+                System.out.println("Created Human: " + human);
+            }
+
+            scanner.close();
+        }
+    }
+
+    static class FFFill {
+        void execute() {
+            String filePath = "Array.txt";
+            try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    String[] parts = line.split(",");
+                    Human human = new Human.HumanBuilder()
+                            .setGender(parts[0])
+                            .setSecondName(parts[1])
+                            .setAge(Integer.parseInt(parts[2]))
+                            .build();
+
+                    System.out.println(human);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
 
