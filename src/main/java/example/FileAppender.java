8a301b2c4b1fce9443f75a0c514d6a579aff5850
Alexander Zhuravlev <warcueid@mail.ru>
2024-10-26 05:18:44 +0400

    Added FileAppender

---
+++ src/main/java/example/FileAppender.java
@@ -0,0 +1,57 @@
+package example;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FileAppender {
+    private final String fileName;
+
+    public FileAppender(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void appendToFile(String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            writer.write(data);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter example format:(xxx,xxx,xxx) or 'exit' for exit: ");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("exit")) {
+                break;
+            }
+
+            System.out.print("Do you want to add this value to the file? (1 - yes, 2 - no):");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            if (choice == 1) {
+                appendToFile(input);
+                System.out.println("Value added to file");
+            } else if (choice == 2) {
+                System.out.println("No value added");
+            } else {
+                System.out.println("Invalid input, please try again");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void main(String[] args) {
+        System.out.print("Enter file name: ");
+        Scanner scanner = new Scanner(System.in);
+        String filename = scanner.nextLine();
+        FileAppender fileAppender = new FileAppender(filename);
+        fileAppender.start();
+    }
+}
