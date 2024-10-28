68fe3180e50e48016659e2e616183e1a4a50249a
Yuliya <yliyalamotkina@yandex.ru>
2024-10-28 15:01:26 +0300

    search_error

--- src/main/java/util/Handle.java
+++ src/main/java/util/Handle.java
@@ -12,34 +12,40 @@ import java.util.List;
 import java.util.Map;
 
 public class Handle {
-	private List entities;
+    private List entities;
 
-	public List getEntities() {
-		return entities;
-	}
+    public List getEntities() {
+        return entities;
+    }
 
-	private final Map<String, Filler> stringFillerHashMap = new HashMap<>();
+    private final Map<String, Filler> stringFillerHashMap = new HashMap<>();
 
-	public Handle() {
-		stringFillerHashMap.put("file", new FileDataFiller());
-		stringFillerHashMap.put("manual", new ManualDataFiller());
-		stringFillerHashMap.put("random", new RandomDataFiller());
-	}
+    public Handle() {
+        stringFillerHashMap.put("file", new FileDataFiller());
+        stringFillerHashMap.put("manual", new ManualDataFiller());
+        stringFillerHashMap.put("random", new RandomDataFiller());
+    }
 
-	public void filler(final String filler, final String entity) throws IOException {
+    public void filler(final String filler, final String entity) throws IOException {
 
-		final Filler importer = stringFillerHashMap.get(filler);
+        final Filler importer = stringFillerHashMap.get(filler);
 
-		entities = importer.fill(entity);
+        entities = importer.fill(entity);
 
-	}
+    }
 
-	public void write() {
-		FileAppender fileAppender = new FileAppender(InputUtils.getString("Введите имя файла: "));
-		for (Object e : entities) {
-			fileAppender.appendToFile(e.toString());
-		}
-	}
+    public void write() {
+        FileAppender fileAppender = new FileAppender(InputUtils.getString("Введите имя файла: "));
+        for (Object e : entities) {
+            fileAppender.appendToFile(e.toString());
+        }
+    }
+
+    public void print() {
+        for (Object e : entities) {
+            System.out.println(e.toString());
+        }
+    }
 
 
 }
